170bc20bded4d2be84da126bc0b53b1ad829dc07
timlossev <65794950+timlossev@users.noreply.github.com>
2022-05-06 14:51:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+
 import json
 import talon
+import re
 from talon import quotations
+from talon import signature
+from talon.signature.bruteforce import extract_signature
+
 from flask import Flask, request, jsonify
 
 talon.init()
@@ -13,7 +18,18 @@ def health_check():
 
 @app.route('/', methods=['POST'])
 def process_message():
-    reply = quotations.extract_from(request.data.decode('utf-8'), request.content_type)
+    question = request.data.decode('utf-8')
+    email_sender = request.sender.decode('utf-8')
+    question = re.sub(r'\\r', '\r', question)
+    question = re.sub(r'\\n', '\n', question)
+    question = re.sub(r'\\t', '\t', question)
+    last_transaction = quotations.extract_from(question, request.content_type)
+
+    if (sender == ""):
+        reply, signature = signature.extract(last_transaction, sender=email_sender)
+    else:
+        reply, signature = extract_signature(last_transaction)
+
     if (request.headers['accept'] == 'application/json'):
         return jsonify({'response': reply})
     else:
